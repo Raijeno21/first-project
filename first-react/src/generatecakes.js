08beb07a37f667b2b1cb1b4a52ab7848d292817f
Raijeno21 <jenopogi221@gmail.com>
2024-09-20 05:48:42 +0800

    version2

--- first-react/src/generatecakes.js
+++ first-react/src/generatecakes.js
@@ -1,45 +1,59 @@
-// Generatecakes.js
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { OrderContext } from './orderContext';
+  // Generatecakes.js
+  import React, { useContext,useEffect } from 'react';
+  import { Link } from 'react-router-dom';
+  import { OrderContext } from './orderContext';
 
-const Generatecakes = ({ loopCake, selectedFlavor }) => {
-  const { orderArray, setOrderArray } = useContext(OrderContext);
+  const Generatecakes = ({ loopCake, selectedFlavor }) => {
+    const { orderArray, setOrderArray } = useContext(OrderContext);
 
-  const filteredCakes = selectedFlavor === 'all'
-    ? loopCake
-    : loopCake.filter(cake => cake.flavor === selectedFlavor);
+    const filteredCakes = selectedFlavor === 'all'
+      ? loopCake
+      : loopCake.filter(cake => cake.flavor === selectedFlavor);
 
-  const handleBuy = (buy) => {
-    const cakeID = buy.target.getAttribute('data-cakeid');
-    setOrderArray(prevOrderArray => [...prevOrderArray, cakeID]);
+    const handleBuy = (buy) => {
+      const cakeID = buy.target.getAttribute('data-cakeid');
+      const searchID = orderArray.find((orderID)=>orderID.cakeID === cakeID );
+      if (!searchID){
+          setOrderArray((currentOrderArray)=>[...currentOrderArray,{cakeID,quantity:1}])
+      }
+      else{
+        setOrderArray((currentOrderArray)=>
+          currentOrderArray.map(order=>(
+           order.cakeID===cakeID ? 
+            {...order, quantity: order.quantity +1}
+            :order
+          ))
+          ) 
+      }
+    };
+    useEffect(()=>{
+      console.log(orderArray)
+    },[orderArray])
+    return (
+      <main>
+        {filteredCakes.map((renderCakes) => (
+          <div className="cake" key={renderCakes.id}>
+            <Link to={`cakes/${renderCakes.id}`}>
+              <img
+                src={renderCakes.image}
+                alt={`Image of ${renderCakes.flavor} cake`}
+                className="cake-images"
+              />
+            </Link>
+            <div className="details">
+              <div>{renderCakes.flavor}</div>
+              <button
+                className="buy-button"
+                data-cakeid={renderCakes.id}
+                onClick={handleBuy}
+              >
+                BUY
+              </button>
+            </div>
+          </div>
+        ))}
+      </main>
+    );
   };
 
-  return (
-    <main>
-      {filteredCakes.map((renderCakes) => (
-        <div className="cake" key={renderCakes.id}>
-          <Link to={`cakes/${renderCakes.id}`}>
-            <img
-              src={renderCakes.image}
-              alt={`Image of ${renderCakes.flavor} cake`}
-              className="cake-images"
-            />
-          </Link>
-          <div className="details">
-            <div>{renderCakes.flavor}</div>
-            <button
-              className="buy-button"
-              data-cakeid={renderCakes.id}
-              onClick={handleBuy}
-            >
-              BUY
-            </button>
-          </div>
-        </div>
-      ))}
-    </main>
-  );
-};
-
-export default Generatecakes;
+  export default Generatecakes;
